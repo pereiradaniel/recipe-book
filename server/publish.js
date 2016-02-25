4e55d16b97e6a8b169ac9997b84371120f6a2eb7
Daniel <truenorthtrainingcompany@gmail.com>
2016-02-25 15:23:58 -0500

    Publish singleRecipe for template Publish and subscribe to a single recipe For displaying single recipe using template helpers on RecipeSingle

--- server/publish.js
+++ server/publish.js
@@ -3,3 +3,10 @@ Meteor.publish('recipes', function(){
 	// Publish the recipes authored by logged in user
 	return Recipes.find({author: this.userId});
 });
+
+Meteor.publish('singleRecipe', function(id){
+	// Check if the id is a string
+	check('id', String);
+	// Publish the recipe for a single recipe view template
+	return Recipes.find({_id: id});
+});
